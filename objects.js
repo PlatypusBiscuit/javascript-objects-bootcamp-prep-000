3f633ce5c81f6921af39cb74176e6054a51fda37
Luke Matheny <lukem17@gmail.com>
2017-11-17 00:15:10 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = {Jeremy: "One Thirst"};
+function updatePlaylist(playlistObj, artist, title) {
+  playlistObj[artist] = title;
+  return playlistObj;
+}
+function removeFromPlaylist(playlistObj, [artist]) {
+  delete playlistObj.artist;
+  return playlistObj;
+}
+console.log(playlist);
+console.log(updatePlaylist(playlist, "Edward Khuil", "Trololo"))
+console.log(removeFromPlaylist(playlist, "Jeremy"))
+
+console.log()
+console.log(removeFromPlaylist({ Kanye: "Gold Digger" }, "Kanye"))
+delete playlist.Jeremy
